cfd80b99b27351d20b3529d29a19e23feabfcf57
MaitiuOCoinne <105300885+MaitiuOCoinne@users.noreply.github.com>
2022-05-13 13:32:55 +0100

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,285 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
+let MusicQuestions = [
+    {
+    numb: 1,
+    question: "What was Fontaines DC's debut album?",
+    answer: "Dogrel",
+    options: [
+      "Dogrel",
+      "A Hero's Death",
+      "Skinty  Fia",
+      "Couldn't tell ye mate"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which Stanglers album contained the single: Golden Brown",
+    answer: "La Folie",
+    options: [
+      "No more Heroes",
+      "La Folie",
+      "Black and White",
+      "Dreamtime"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Who is the frontman of the strokes",
+    answer: "Julian Casablancas",
+    options: [
+      "Joe McDonnel",
+      "Patsy V",
+      "Julian Casablancas",
+      "Ted Sharkey"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What year was Bobby Womack's Across 110th Street released",
+    answer: "1973",
+    options: [
+      "1916",
+      "1981",
+      "1962",
+      "1973"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Bicep are from what Irish city?",
+    answer: "Belfast",
+    options: [
+      "Derry",
+      "Belfast",
+      "Dublin",
+      "Cork"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
+
+let MovieQuestions = [
+    {
+    numb: 1,
+    question: "Which of the following is not a film directed by Martin McDonagh ",
+    answer: "The Guard",
+    options: [
+      "The Guard",
+      "In Bruges",
+      "Three Billboards",
+	  "Seven Psychopaths"
+    ]
+  },
+    {
+    numb: 2,
+    question: "In what year was Goodfellas released?",
+    answer: "1990",
+    options: [
+      "1985",
+      "2001",
+      "1990",
+      "2026"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Who directed Se7en",
+    answer: "David Fincher",
+    options: [
+      "David lynch",
+      "Paul Giamtti",
+      "David Fincher",
+      "Ed wood"
+    ]
+  },
+    {
+    numb: 4,
+    question: "what is the name of the character played by Christoph Waltz in Django Unchained",
+    answer: "King Schultz",
+    options: [
+      "Django",
+      "Hans Landa",
+      "King Schultz",
+      "Mr Teeth"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Did ye see that film with yer man in it?",
+    answer: "what are you doing in my house?",
+    options: [
+      "Sir, I am blind.",
+      "No",
+      "Yes",
+      "What are you doing in my house?"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];let TVQuestions = [
+    {
+    numb: 1,
+    question: "Is there a man staring at you with a T-Shirt saying I sahot JR",
+    answer: "BIRD!!",
+    options: [
+      "oh aye",
+      "No",
+      "What",
+	  "BIRD!!"
+    ]
+  },
+    {
+    numb: 2,
+    question: "How many series of Breaking Bad were there?",
+    answer: "5",
+    options: [
+      "2",
+      "6",
+      "5",
+      "32"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Where was kings road in Game of thrones Filmed",
+    answer: "The Dark Hedges",
+    options: [
+      "Giants Causeway",
+      "the M12",
+      "The Dark Hedges",
+      "Green screen"
+    ]
+  },
+    {
+    numb: 4,
+    question: "How many Doctors have their been in doctor who?",
+    answer: "13",
+    options: [
+      "19",
+      "7",
+      "21",
+      "13"
+    ]
+  },
+    {
+    numb: 5,
+    question: "How is Danny Dyer still on TV?",
+    answer: "Pact with satan",
+    options: [
+      "Talent",
+      "He knows where the bodies are buried",
+      "Pact with satan",
+      "Yes"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
